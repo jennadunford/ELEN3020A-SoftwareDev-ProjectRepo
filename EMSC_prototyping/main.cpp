5540bcb2124ff5bd4e185162cf02c1587ee5c4b4
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-06-08 18:00:59 +0200

    Update main.cpp

--- EMSC_prototyping/main.cpp
+++ EMSC_prototyping/main.cpp
@@ -23,6 +23,44 @@ extern "C" {
  return (a+b);
  }
 
+  EMSCRIPTEN_KEEPALIVE int genCostPerKmPetrol(){
+    return 0;
+  }
+
+    EMSCRIPTEN_KEEPALIVE int genCostPerKmElec(){
+      return 0;
+    }
+
+    EMSCRIPTEN_KEEPALIVE int genEnergyUsagePeHrPetrol(){
+      return 0;
+    }
+
+    EMSCRIPTEN_KEEPALIVE int genEnergyUsagePerHrElec(){
+      return 0;
+    }
+
+    EMSCRIPTEN_KEEPALIVE string showConversionPetrolKWH(string carType){
+      //string carType will be fed into a database which gives us fuel consumption in litres per 100km
+          float fuel;  //fuel consumption in litres per 100km
+          float fkWhConst = 8.902598;
+          float fConvert;
+
+          fConvert = fuel * fkWhConst; // l/100km to kWh/100km
+              
+              float wh //energy consumption in watt-hour per km
+              float fKWh;
+              float fKWhconst = 0.1;
+
+          fKWh =  wh * fKWhconst; //  wh/km to kWh/100km
+
+
+
+
+      return "hello";
+    }
+
+
+
 #ifdef __cplusplus
 }
 #endif
