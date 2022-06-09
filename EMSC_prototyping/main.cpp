7f8e4a2d66c191488848902725540b8f12778283
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-06-09 11:15:46 +0200

    Attempting to get database and emcc to run together (unsucessful)

--- EMSC_prototyping/main.cpp
+++ EMSC_prototyping/main.cpp
@@ -3,6 +3,16 @@
 #include <emscripten/emscripten.h>
 #include <iostream>
 
+//LIBRARIES FOR DATABASE TO WORK
+// #include <stdlib.h>
+// #include <iostream>
+// #include <mysql_connection.h>
+// #include <driver.h>
+// #include <exception.h>
+// #include <resultset.h>
+// #include <statement.h>
+//END OF LIBRARIES FOR DATABASE TO WORK
+
 using namespace std;
 
 int main (void)
@@ -47,20 +57,43 @@ extern "C" {
 
           fConvert = fuel * fkWhConst; // l/100km to kWh/100km
               
-              float wh //energy consumption in watt-hour per km
+              float wh; //energy consumption in watt-hour per km
               float fKWh;
               float fKWhconst = 0.1;
 
           fKWh =  wh * fKWhconst; //  wh/km to kWh/100km
-
-
-
-
       return "hello";
     }
-
-
 
 #ifdef __cplusplus
 }
 #endif
+
+
+  // EMSCRIPTEN_KEEPALIVE void connectDatabase(){
+  // Driver *driver;
+  // Connection *con;
+  // Statement *stmt;
+  // ResultSet  *res;
+
+  // driver = get_driver_instance();
+  // con = driver->connect("remotemysql.com","yn7m0SUz0Y","LWRebclM6g");
+
+  // if(con->isValid()){
+  //   std::cout << "The connection is valid!" << std::endl;
+  // }
+
+  // stmt = con->createStatement();
+  // stmt->execute("USE yn7m0SUz0Y");
+  // res = stmt->executeQuery("SELECT Make, Model, Id, Class, Petrol_Usage FROM TransportTypes;");
+  // std:: cout << "Make - Model - Id - Class - Petrol Usage" << std::endl;
+  // while(res->next()){   
+  //   std::cout << res->getString("Make") << " - ";
+  //   std::cout << res->getString("Model") << " - ";
+  //   std::cout << res->getString("Id") << " - ";
+  //   std::cout << res->getString("Class") << " - ";
+  //   std::cout << res->getDouble("Petrol_Usage") << std::endl;
+  // }
+  // }
+
+
--- tester.cpp
+++ tester.cpp
@@ -6,8 +6,10 @@
 #include <resultset.h>
 #include <statement.h>
 
+//#include <emscripten/emscripten.h>
+
 using namespace sql;
-int main(void){
+ void showDatabase(){
   Driver *driver;
   Connection *con;
   Statement *stmt;
@@ -31,6 +33,35 @@ int main(void){
     std::cout << res->getString("Class") << " - ";
     std::cout << res->getDouble("Petrol_Usage") << std::endl;
   }
+ }
+int main(void){
+    showDatabase();
 
   return 0;
+  // Driver *driver;
+  // Connection *con;
+  // Statement *stmt;
+  // ResultSet  *res;
+
+  // driver = get_driver_instance();
+  // con = driver->connect("remotemysql.com","yn7m0SUz0Y","LWRebclM6g");
+
+  // if(con->isValid()){
+  //   std::cout << "The connection is valid!" << std::endl;
+  // }
+
+  // stmt = con->createStatement();
+  // stmt->execute("USE yn7m0SUz0Y");
+  // res = stmt->executeQuery("SELECT Make, Model, Id, Class, Petrol_Usage FROM TransportTypes;");
+  // std:: cout << "Make - Model - Id - Class - Petrol Usage" << std::endl;
+  // while(res->next()){   
+  //   std::cout << res->getString("Make") << " - ";
+  //   std::cout << res->getString("Model") << " - ";
+  //   std::cout << res->getString("Id") << " - ";
+  //   std::cout << res->getString("Class") << " - ";
+  //   std::cout << res->getDouble("Petrol_Usage") << std::endl;
+  // }
+
+
 }
+
