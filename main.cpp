229c019fde1cd1ce5b1dc3f35d3f162a692a45f1
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-06-09 05:56:35 +0200

    Added mysql files

--- main.cpp
+++ main.cpp
@@ -1,17 +1,48 @@
 //Put C code here
 #include <iostream>
 #include "comparisons.h"
+#include<SQLAPI.h>  
 
 using namespace std;
 
 int main (void)
 {
-      // Create and open a text file
-  ofstream MyFile("//wsl.localhost/Ubuntu/home/jennadunford/ELEN3020A-SoftwareDev-ProjectRepo/JavascriptOutputTesting/text.exe");
-
-  // Write to the file
-  MyFile << "Trying to get this text file to be created through javascript";
-
-  // Close the file
-  MyFile.close();
+ // create connection object to connect to database
+    SAConnection con;
+    try
+    {
+        // connect to database
+        // in this example, it is Oracle,
+        // but can also be Sybase, Informix, DB2
+        // SQLServer, InterBase, SQLBase and ODBC
+        con.Connect ("yn7m0SUz0Y",    // database name
+                     "yn7m0SUz0Y",  // user name
+                     "LWRebclM6g",  // password
+                     SQLServer); //Oracle Client
+        printf("We are connected!\n");
+ 
+        // Disconnect is optional
+        // autodisconnect will occur in destructor if needed
+        con.Disconnect();
+        printf("We are disconnected!\n");
+    }
+ 
+    catch(SAException &amp; x)
+    {
+        // SAConnection::Rollback()
+        // can also throw an exception
+        // (if a network error for example),
+        // we will be ready
+        try
+        {
+            // on error rollback changes
+            con.Rollback ();
+        }
+        catch(SAException &amp;)
+        {
+        }
+        // print error message
+        printf("%s\n", (const char*)x.ErrText());
+    }
+    return 0;
 }
