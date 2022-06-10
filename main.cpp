ec5051de59fbb43b3aa850abbed340a8c65a5537
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-06-10 13:13:36 +0200

    updating scripts

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
-#include <stdlib.h>
-#include <iostream>
+//Put C code here
 #include <stdio.h>
 #include <emscripten/emscripten.h>
-
+#include <iostream>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <cmath>
 
 using namespace std;
 
@@ -10,6 +15,7 @@ int main (void)
 {
 
 }
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -18,43 +24,53 @@ extern "C" {
  return (a+b);
  }
 
-  EMSCRIPTEN_KEEPALIVE int genCostPerKmPetrol(){
-    return 0;
+  EMSCRIPTEN_KEEPALIVE float genCostPerKmPetrol(float consumptionVal){
+    float petrolCost = 23.845;
+    float costPerKm = ((consumptionVal)*(petrolCost))/100;
+    return costPerKm;
   }
 
-    EMSCRIPTEN_KEEPALIVE int genCostPerKmElec(){
+    /*EMSCRIPTEN_KEEPALIVE int genCostPerKmElec(){
       return 0;
+    }*///This function is not needed, the value is constant
+
+    EMSCRIPTEN_KEEPALIVE float genEnergyUsagePeKmPetrol(float consumptionVal){
+      float fkWhConst = 8.902598;
+      float fConvert;
+      fConvert = consumptionVal * fkWhConst; // l/100km to kWh/100km
+      float kJPerKm = 3600*((fConvert)/100);
+      return kJPerKm;
     }
 
-    EMSCRIPTEN_KEEPALIVE int genEnergyUsagePeHrPetrol(){
+    /*EMSCRIPTEN_KEEPALIVE float genEnergyUsagePerKmElec(){
       return 0;
-    }
+    }*///this value will be constant so a function is not needed
 
-    EMSCRIPTEN_KEEPALIVE int genEnergyUsagePerHrElec(){
-      return 0;
-    }
 
-    EMSCRIPTEN_KEEPALIVE string showConversionPetrolKWH(string carType){
+  EMSCRIPTEN_KEEPALIVE float sConversionPetrolKWH(float consumptionVal){
       //string carType will be fed into a database which gives us fuel consumption in litres per 100km
-          float fuel;  //fuel consumption in litres per 100km
+     //fuel consumption in litres per 100km
           float fkWhConst = 8.902598;
           float fConvert;
 
-          fConvert = fuel * fkWhConst; // l/100km to kWh/100km
+          fConvert = consumptionVal * fkWhConst; // l/100km to kWh/100km
               
-              float wh; //energy consumption in watt-hour per km
-              float fKWh;
+              float wh = 0; //energy consumption in watt-hour per km
+              float fKWh = 0;
               float fKWhconst = 0.1;
 
           fKWh =  wh * fKWhconst; //  wh/km to kWh/100km
-      return "hello";
+          
+      return round(fConvert);
     }
+
 
   
 
 #ifdef __cplusplus
 }
 #endif
+
 
 
 
